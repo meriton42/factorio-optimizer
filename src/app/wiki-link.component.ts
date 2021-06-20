ad80a132cd475a711ba6107ac66fd9bc36d87fd7
meriton <meriton@somewhere.org>
2021-06-20 15:45:58 +0200

    fix rail icon

--- src/app/wiki-link.component.ts
+++ src/app/wiki-link.component.ts
@@ -4,11 +4,16 @@ import { Component, Input } from '@angular/core';
 	selector: 'wiki-link',
 	template: `
 		<a [href]="'https://wiki.factorio.com/' + (item | wikiPage)">
-			<img [src]="'https://wiki.factorio.com/images/' + (item | wikiPage) + '.png'" width="16">
+			<img [src]="'https://wiki.factorio.com/images/' + (image | wikiPage) + '.png'" width="16">
 		</a>
 	`
 })
 export class WikiLinkComponent {
 	@Input()
 	item: string;
+
+	get image() {
+		const {item} = this;
+		return item == 'rail' ? 'straightRail' : item;
+	}
 }
