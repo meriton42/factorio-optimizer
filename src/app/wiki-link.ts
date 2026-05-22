1d2f6ef3fa4794002b1a26518ff3604d2b0ca4af
meriton <meriton@somewhere.org>
2026-05-23 00:20:24 +0200

    goodbye layout tables, welcome grid

--- src/app/wiki-link.ts
+++ src/app/wiki-link.ts
@@ -6,9 +6,9 @@ import { WikiPagePipe } from './pipes';
 	imports: [WikiPagePipe],
 	template: `
 		<a [href]="'https://wiki.factorio.com/' + (item() | wikiPage)">
-			<img [src]="'https://wiki.factorio.com/images/' + (image | wikiPage) + '.png'" width="16">
+			<img [src]="'https://wiki.factorio.com/images/' + (image | wikiPage) + '.png'" width="16" style="display: block;">
 		</a>
-	`
+	`,
 })
 export class WikiLink {
 	item = input.required<string>();
